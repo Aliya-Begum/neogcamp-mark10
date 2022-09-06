9eac376b034423f2a2e68224243032a0de2b55cb
Aliya-Begum <aliyaansari0604@gmail.com>
2022-09-06 12:20:08 -0700

    initial commit

--- app.js
+++ app.js
@@ -2,23 +2,38 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
 var btnCheck = document.querySelector("#check-button");
 var errMessage = document.querySelector("#error-message");
-var availableNotesWeHave = document.querySelectorAll(".available-notes");
-console.log(availableNotes);
+var numberOfNotes = document.querySelectorAll(".no-of-notes");
 
-var availableNotes =[2000, 500, 100, 20, 10, 5, 1];
 
-btnCheck.addEventListener("click", function getValidateBillAmountAndCashGiven(){
-   
-    if(billAmount.value>0){
+var availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+
+btnCheck.addEventListener("click", function checkClickHandler() {
+    errMessage.style.display = "none";
+    if (billAmount.value > 0) {
         if(cashGiven.value>=billAmount.value){
-            const amountToBeReturned = (cashGiven.value- billAmount.value); //2060-50 = 2010
-             calculateNoOfNotes(amountToBeReturned);
-        }else {
-             errMessage.innerText = "cash amount should be greater than bill amount";
+           var amountToBeReturned = cashGiven.value - billAmount.value;
+           calculateNoOfNotes(amountToBeReturned);
+
         }
-    
-    }else {
-        errMessage.innerText = "Invallid bill Amount";
-    
-    }}
-)
+        else{
+            showErrorMessage("cash given should atleast be greater than or equal to bill amount");
+        }
+    }
+    else {
+       showErrorMessage("bill amount should be greater than zero");
+    }
+})
+
+function showErrorMessage (message){
+    errMessage.style.display = "block";
+        errMessage.innerText = message;
+}
+
+function calculateNoOfNotes(amountToBeReturned){
+    for(let i= 0; availableNotes.length; i++){
+        var noOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        numberOfNotes[i].innerText = noOfNotes; 
+    }
+}
